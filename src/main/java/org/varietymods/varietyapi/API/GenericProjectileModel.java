9aa6bf56e00e990eb3333a80b5dbdf8f3c791bd5
Colin Vaughn <Colinvaughn01@gmail.com>
2023-05-26 17:59:17 -0400

    projectiles

---
+++ src/main/java/org/varietymods/varietyapi/API/GenericProjectileModel.java
@@ -0,0 +1,38 @@
+package org.varietymods.varietyapi.API;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+public class GenericProjectileModel<T extends LivingEntity & IAnimatable> extends AnimatedGeoModel<T> {
+    private final String modId;
+    private final String modelPath;
+    private final String texturePath;
+    private final String animationPath;
+
+    public GenericProjectileModel(String modId, String modelPath, String texturePath, String animationPath) {
+        this.modId = modId;
+        this.modelPath = modelPath;
+        this.texturePath = texturePath;
+        this.animationPath = animationPath;
+    }
+
+    @Override
+    public Identifier getModelResource(T object) {
+        return new Identifier(modId, "geo/"+ modelPath+".json");
+    }
+
+    @Override
+    public Identifier getTextureResource(T object) {
+        return new Identifier(modId, "textures/entity/"+texturePath+".png");
+    }
+
+    @Override
+    public Identifier getAnimationResource(T animatable) {
+        if ("null".equals(animationPath)) {
+            return null;
+        }
+        return new Identifier(modId,"animations/"+animationPath+".json");
+    }
+}
---
+++ src/main/java/org/varietymods/varietyapi/API/GenericProjectileRenderer.java
@@ -0,0 +1,46 @@
+package org.varietymods.varietyapi.API;
+
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.renderers.geo.GeoProjectilesRenderer;
+
+public class GenericProjectileRenderer<T extends ProjectileEntity & IAnimatable> extends GeoProjectilesRenderer<T> {
+
+    private final String modId;
+    private final String texturePath;
+    private final boolean isTranslucent;
+    private final boolean hasGlowLayer;
+    public GenericProjectileRenderer(EntityRendererFactory.Context ctx, AnimatedGeoModel<T> modelProvider, String modId, String texturePath
+                                  , boolean isTranslucent, boolean hasGlowLayer) {
+        super(ctx, modelProvider);
+        this.modId = modId;
+        this.texturePath = texturePath;
+        this.isTranslucent = isTranslucent;
+        this.hasGlowLayer = hasGlowLayer;
+
+
+        this.shadowRadius = 0.4f;
+        if(hasGlowLayer) {
+            // this.addLayer(new LayerGlowingAreasGeo<>(this, getGeoModelProvider()::getTextureResource, getGeoModelProvider()::getModelResource, RenderLayer::getEntityTranslucentEmissive));
+        }
+    }
+    @Override
+    public Identifier getTextureResource(T instance) {
+        return new Identifier(modId, "textures/entity/" + texturePath + ".png");
+    }
+    @Override
+    public RenderLayer getRenderType(T animatable, float partialTicks, MatrixStack stack,
+                                     VertexConsumerProvider renderTypeBuffer, VertexConsumer vertexBuilder,
+                                     int packedLightIn, Identifier textureLocation) {
+
+        return isTranslucent ? RenderLayer.getEntityTranslucent(getTextureResource(animatable))
+                : super.getRenderType(animatable, partialTicks, stack, renderTypeBuffer, vertexBuilder, packedLightIn, textureLocation);
+    }
+}
