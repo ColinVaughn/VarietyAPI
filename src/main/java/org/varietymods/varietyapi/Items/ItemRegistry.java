638fd44c276d2dadc24afc9bf6e472e0df5610bb
Colin Vaughn <Colinvaughn01@gmail.com>
2023-06-10 08:41:25 -0400

    Slightly improved API

--- src/main/java/org/varietymods/varietyapi/Items/ItemRegistry.java
+++ src/main/java/org/varietymods/varietyapi/Items/ItemRegistry.java
@@ -1,7 +1,10 @@
 package org.varietymods.varietyapi.Items;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -15,9 +18,16 @@ public class ItemRegistry {
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(VarietyAPI.MOD_ID, name), item);
     }
+    public static void addItemsToItemGroup() {
+        addToItemGroup(ModItemGroup.VARIETY_MODS, NETITEM);
+    }
 
+    private static void addToItemGroup(ItemGroup group, Item item) {
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
 
     public static void registerModItems() {
         VarietyAPI.LOGGER.debug("Registering Mod Items for " + VarietyAPI.MOD_ID);
+        addItemsToItemGroup();
     }
 }
--- src/main/java/org/varietymods/varietyapi/client/VarietyAPIClient.java
+++ src/main/java/org/varietymods/varietyapi/client/VarietyAPIClient.java
@@ -5,12 +5,6 @@ import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.TooltipComponentCallback;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.block.FluidRenderer;
-import net.minecraft.client.render.entity.EntityRendererFactory;
-import org.varietymods.varietyapi.API.GenericModel;
-import org.varietymods.varietyapi.API.GenericRenderer;
-import org.varietymods.varietyapi.Entities.ModEntities;
-import org.varietymods.varietyapi.VarietyAPI;
 
 public class VarietyAPIClient implements ClientModInitializer {
     @Override
@@ -18,8 +12,5 @@ public class VarietyAPIClient implements ClientModInitializer {
 
         HudRenderCallback.EVENT.register(new VarietyHudOverlay(MinecraftClient.getInstance()));
         TooltipComponentCallback.EVENT.register(new VarietyTooltipCallback());
-        EntityRendererRegistry.register(ModEntities.LEVIATHAN, (EntityRendererFactory.Context ctx) ->
-                new GenericRenderer<>(ctx, new GenericModel(VarietyAPI.MOD_ID,"spermwhale.geo","spermwhale_texture","spermwhale.animation"),VarietyAPI.MOD_ID, "spermwhale_texture", 1.1f,1.2f, false,false)
-        );
     }
 }
